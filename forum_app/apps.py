d0879799a53d0be5bfbac9a20a734357cd1ffaa7
unknown <kottugoroshko232@gmail.com>
2025-02-26 12:04:39 +0200

    start new project again

--- forum_app/apps.py
+++ forum_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PortalConfig(AppConfig):
+class ForumAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portal'
+    name = 'forum_app'
